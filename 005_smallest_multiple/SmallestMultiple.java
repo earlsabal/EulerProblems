9afab349d31fe57b0623e14cf90843e71b0bfec7
home <home@homes-MBP-3.att.net>
2017-07-16 22:25:05 -0700

    Solved problem 5

---
+++ 005_smallest_multiple/SmallestMultiple.java
@@ -0,0 +1,21 @@
+public class SmallestMultiple {
+
+	public int smallestMultiple(int highestDivisor) {
+		int currentNumber = highestDivisor;
+		while (isMultiple(currentNumber, highestDivisor) != true) {
+			currentNumber++;
+		}
+		return currentNumber;
+	}
+
+	public boolean isMultiple(int number,int max) {
+		for (int i = max; i > 0; i--)
+			if (number % i != 0) { return false; }
+		return true;
+	}
+
+	public static void main(String[] args) {
+		SmallestMultiple test = new SmallestMultiple();
+		System.out.println(test.smallestMultiple(20));
+  }
+}
