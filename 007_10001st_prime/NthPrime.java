c36bb5ed1734068aa7649a3c28daf41429223a72
home <home@homes-MBP-3.att.net>
2017-07-16 22:55:24 -0700

    Solved problem 7

---
+++ 007_10001st_prime/NthPrime.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class NthPrime {
+  public int findPrime(int number) {
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+    int targetIndex = number - 1;
+    int currentNumber = 2;
+    while (primes.size() < number ) {
+      if ( isPrime(currentNumber, primes) == true ) { primes.add(currentNumber); }
+      currentNumber++;
+    }
+    int nthPrime = primes.get( targetIndex );
+    return nthPrime;
+  }
+
+  public boolean isPrime(int num, ArrayList<Integer> primes) {
+    for (int prime : primes) {
+      if (num % prime == 0) { return false; }
+      if (prime > (num / 2)) { return true; }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    NthPrime test = new NthPrime();
+    System.out.println(test.findPrime(10001));
+  }
+}
